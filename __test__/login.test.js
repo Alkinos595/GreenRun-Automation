6446e65dbd81e322f980699c9c9ee206caa6c792
Alberto <foinquinosalberto@gmail.com>
2023-02-17 16:21:06 -0400

    Update the login test, add new lib files

--- __test__/login.test.js
+++ __test__/login.test.js
@@ -1,31 +1,27 @@
-const puppeteer = require('puppeteer');
 const pick = require('../lib/selectors');
 const make = require('../lib/helpers');
-const iphone = puppeteer.KnownDevices['iPhone SE'];
-describe('Login with phone test', () => {
-    let browser, page;
-    beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless: false,
-            devtools: true,
-        });
-        page = await browser.newPage();
-        await page.emulate(iphone);
-    }, 30000);
+const config = require('../lib/config');
+const data = require('../lib/data');
 
-    afterAll(async () => {
-        await browser.close();
-    });
-    //*----------------------------------------------------------------------------------------------------
-    test('Go to Page', async () => {
-        await page.goto('https://staging.greenrun.com/');
-        const url = await page.url();
-        console.log('Visitaste: ' + url);
+describe('Login with phone test', () => {
+    config.set;
+    beforeAll(config.before, 30000);
+    afterAll(config.after);
+
+    test('Go to Page Validation', async () => {
+        const url = await make.getUrl();
+        expect(url).toMatch('https://staging.greenrun.com/');
     }, 30000);
 
     test('Fill the form', async () => {
-        console.log('Selector: ' + pick.loginButton);
         await make.clickX(page, pick.loginButton);
         await make.clickX(page, pick.phoneButton);
-    }, 30000);
+        await make.click(page, pick.inputPhone);
+        await make.eraseChar(3);
+        await make.type(page, pick.inputPhone, data.phone);
+        await make.click(page, pick.submitButton);
+        await make.click(page, pick.inputOTP);
+        await make.type(page, pick.inputOTP, data.otp, { delay: 100 });
+        await make.wait(10000);
+    }, 60000);
 });
